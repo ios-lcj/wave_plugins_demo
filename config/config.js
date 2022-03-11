f0337405139b9e39da8364db2a4011e75831e988
chjli <ios_li@icloud.com>
2022-03-11 15:15:13 +0800

    fixed bugs

--- config/config.js
+++ config/config.js
@@ -2,53 +2,42 @@ import { defineConfig } from 'umi';
 import routes from './routes';
 
 export default defineConfig({
-    base: '/',
-    history: {
-        type: 'hash',
-    },
-    publicPath: './',
-    hash: true,
-    antd: {},
-    dva: {
-        hmr: true,
-    },
-    // https://umijs.org/zh-CN/plugins/plugin-locale
-    locale: {
-        // default zh-CN
-        default: 'en_US',
-        antd: true,
-        baseNavigator: false,
-        baseSeparator: '_',
-    },
-    dynamicImport: {
-        loading: '@/components/PageLoading/index',
-    },
-    targets: {
-        ie: 11,
-    },
-    routes,
-    theme: {
-        'primary-color': '#1890ff',
-    },
-    title: false,
-    ignoreMomentLocale: true,
-    // proxy,
-    manifest: {
-        basePath: '/',
-    },
-    nodeModulesTransform: {
-        type: 'none',
-    },
-    webpack5: {},
-    chainWebpack(memo) {
-        memo.output.filename('js/[name].[hash:8].js');
-        memo.output.chunkFilename('js/[name].[hash:8].js');
-        memo.plugin('extract-css').tap(args => [
-            {
-                ...(args[0] || {}),
-                filename: 'css/[name].[hash:8].css',
-                chunkFilename: 'css/[name].[hash:8].css',
-            }
-        ])
-    }
+  base: '/',
+  history: {
+    type: 'hash',
+  },
+  publicPath: './',
+  hash: true,
+  dva: {
+    hmr: true,
+  },
+  dynamicImport: {},
+  targets: {
+    ie: 11,
+  },
+  routes,
+  theme: {
+    'primary-color': '#1890ff',
+  },
+  title: false,
+  ignoreMomentLocale: true,
+  // proxy,
+  manifest: {
+    basePath: '/',
+  },
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  webpack5: {},
+  chainWebpack(memo) {
+    memo.output.filename('js/[name].[hash:8].js');
+    memo.output.chunkFilename('js/[name].[hash:8].js');
+    memo.plugin('extract-css').tap((args) => [
+      {
+        ...(args[0] || {}),
+        filename: 'css/[name].[hash:8].css',
+        chunkFilename: 'css/[name].[hash:8].css',
+      },
+    ]);
+  },
 });
